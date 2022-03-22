98bb4d4b07c7f055472d45eb86df433ccd6ab9f8
Greyluck <36477858+Greyluck@users.noreply.github.com>
2022-03-22 13:43:19 -0300

    Initial commit

---
+++ IonateTest/src/main/java/com/example/ionate/App.java
@@ -0,0 +1,14 @@
+package com.example.ionate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
---
+++ IonateTest/src/test/java/com/example/ionate/launchbrowser.java
@@ -0,0 +1,11 @@
+package com.example.ionate;
+
+
+import java.util.NoSuchElementException;
+
+public class launchbrowser {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Test case completed without errors");
+    }
+}
