9ed62939a58b55b6d8a5482fd4898b432e9a6fff
Greyluck <36477858+Greyluck@users.noreply.github.com>
2022-03-22 19:02:24 -0300

    Chromedriver is working

--- IonateTest/src/main/java/com/example/ionate/LaunchBrowser.java
+++
@@ -1,8 +0,0 @@
-package com.example.ionate;
-
-public class LaunchBrowser {
-    public static boolean executeMainTest(){
-        System.out.println("Test case completed without errors");
-        return true;
-    }
-}
---
+++ IonateTest/src/test/java/com/ionate/AppTest.java
@@ -0,0 +1,37 @@
+package com.ionate;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Assert;
+
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     * @param testName name of the test case
+     */
+    public AppTest( String testName ) throws InterruptedException {
+        super( testName );
+
+        boolean ionateWebPageResult = App.returnWebTestResult();
+        Assert.assertTrue("Test failed. ", ionateWebPageResult);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
